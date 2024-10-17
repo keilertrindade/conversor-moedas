1c4e4042e54f17f6df3a994db3fa422d2c726d39
Keiler Trindade <keiler.trindade@hotmail.com>
2024-10-17 16:12:49 -0300

    adicionado versão final do conversor

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int opcao = -1;
+        double valor = 0;
+        //Request request = new Request();
         Scanner leitura = new Scanner(System.in);
         do{
-            //Runtime.getRuntime().exec("cls");
+            System.out.println("************************************");
             System.out.println("1) Dólar =>> Peso argentino");
             System.out.println("2) Peso argentino =>> Dólar");
             System.out.println("3) Dólar =>> Real brasileiro");
@@ -13,32 +15,74 @@ public class Main {
             System.out.println("5) Dólar =>> Peso colombiano");
             System.out.println("6) Peso colombiano =>> Dólar");
             System.out.println("7) Sair");
-            System.out.println("Escolha uma opção válida: ");
-            System.out.println("************************************");
+            System.out.print("Escolha uma opção: ");
+            //System.out.println("************************************");
             opcao = leitura.nextInt();
 
-            if(opcao < 7 && opcao > 0){
+            /*if(opcao < 7 && opcao > 0){
 
-            }
+            } */
 
             switch (opcao){
                 case 1:
-                    System.out.println("1");
+                    System.out.println("Insira o valor a ser convertido: ");
+                    valor = leitura.nextDouble();
+                    try {
+                        double valorConvertido = Request.getExchangeRate("USD","ARS",valor);
+                        System.out.println(valor+" USD = " + valorConvertido + " ARS");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
                 case 2:
-                    System.out.println("2");
+                    System.out.println("Insira o valor a ser convertido: ");
+                    valor = leitura.nextDouble();
+                    try {
+                        double valorConvertido = Request.getExchangeRate("ARS","USD",valor);
+                        System.out.println(valor+" ARS = " + valorConvertido + " USD");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
                 case 3:
-                    System.out.println("3");
+                    System.out.println("Insira o valor a ser convertido: ");
+                    valor = leitura.nextDouble();
+                    try {
+                        double valorConvertido = Request.getExchangeRate("USD","BRL",valor);
+                        System.out.println(valor+" USD = " + valorConvertido + " BRL");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
                 case 4:
-                    System.out.println("4");
+                    System.out.println("Insira o valor a ser convertido: ");
+                    valor = leitura.nextDouble();
+                    try {
+                        double valorConvertido = Request.getExchangeRate("BRL","USD",valor);
+                        System.out.println(valor+" BRL = " + valorConvertido + " USD");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
                 case 5:
-                    System.out.println("5");
+                    System.out.println("Insira o valor a ser convertido: ");
+                    valor = leitura.nextDouble();
+                    try {
+                        double valorConvertido = Request.getExchangeRate("USD","COP",valor);
+                        System.out.println(valor+" USD = " + valorConvertido + " COP");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
                 case 6:
-                    System.out.println("6");
+                    System.out.println("Insira o valor a ser convertido: ");
+                    valor = leitura.nextDouble();
+                    try {
+                        double valorConvertido = Request.getExchangeRate("COP","USD",valor);
+                        System.out.println(valor+" COP = " + valorConvertido + " USD");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
                 case 7:
                     System.out.println("Saindo do programa...");
@@ -47,8 +91,5 @@ public class Main {
                     System.out.println("Escolha uma opção válida!");
             }
         }while (opcao != 7);
-
-
-
     }
 }
--- src/Request.java
+++ src/Request.java
@@ -1,5 +1,6 @@
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,28 +9,37 @@ import java.net.http.HttpResponse;
 
 public class Request {
 
-    private static final String BASE_URL = "https://api.exchangerate.host";
+    private static final String BASE_URL = "https://v6.exchangerate-api.com/v6/";
+    private static final String API_KEY = "859ecc89b91c358e082ad6d6";
 
-    public static double getExchangeRate(String from, String to) throws Exception {
+    public Request() {
+    }
+
+    public static double getExchangeRate(String from, String to, double valor) throws Exception {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(BASE_URL
-                        + "/latest?base=" + from))
+                .uri(URI.create(BASE_URL + API_KEY
+                        + "/latest/" + from))
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
 
-        // Aqui você precisará parsear o JSON da resposta para extrair a taxa desejada
-        // Utilize uma biblioteca como Jackson ou Gson para isso.
-        // Por exemplo, usando Jackson:
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.body());
-        JsonNode rates = root.get("rates");
-        return rates.get(to).asDouble();
-    }
+        JsonParser parser = new JsonParser();
+        JsonElement jsonTree = parser.parse(json);
+        JsonObject jsonObject = jsonTree.getAsJsonObject();
+        JsonObject conversionRates = jsonObject.getAsJsonObject("conversion_rates");
 
-    public static void main(String[] args) throws Exception {
-        double exchangeRate = getExchangeRate("USD", "BRL");
-        System.out.println("1 USD = " + exchangeRate + " BRL");
+        //double fromValue = conversionRates.get(from).getAsDouble();
+        double toValue = conversionRates.get(to).getAsDouble();
+        return (valor*toValue);
+        /*
+        ARS - Peso argentino
+        BOB - Boliviano boliviano
+        BRL - Real brasileiro
+        CLP - Peso chileno
+        COP - Peso colombiano
+        USD - Dólar americano
+        */
     }
     }
